20231c153223a4d8631b802913190a5c99924183
mir-sakhawat <sakhawat.code.info@gmail.com>
2025-01-02 06:54:19 +0600

    first commit

---
+++ mir_live_exam.dart
@@ -0,0 +1,12 @@
+void main() {
+  triangle();
+}
+
+void triangle() {
+  double base = 10;
+  double height = 5;
+
+  double area = 0.5 * base * height;
+
+  print("The area of the triangle is: $area");
+}
